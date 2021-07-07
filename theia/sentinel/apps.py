d541fd11800e55df74bbad8c4e5cd3326808e78f
Calem Roelofs <davidnullsworth@protonmail.com>
2021-07-07 20:52:21 +0100

    black formatting

--- theia/sentinel/apps.py
+++ theia/sentinel/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class SentinelConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sentinel'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "sentinel"
