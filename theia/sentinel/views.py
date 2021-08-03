ca2d7a0f23f90417d48c7bd835413e48ede0b758
Calem Roelofs <calem.roelofs@viatel.com>
2021-08-03 17:20:49 +0100

    Added placeholders for views

--- theia/sentinel/views.py
+++ theia/sentinel/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+def server_overview():
+    pass
+
+
+def server_view():
+    pass
+
+
+def server_logs():
+    pass
+
+
+def scan_logs():
+    pass
+
+
+def scan_view():
+    pass
+
+
+def alerts_view():
+    pass
+
+
+def alerts_logs():
+    pass
