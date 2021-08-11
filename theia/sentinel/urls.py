1ac45771eb3a4d389a4778544b0d82617b8a7642
Calem Roelofs <calemroelofs@outlook.ie>
2021-08-11 17:52:57 +0000

    Added serview view and server overview backend

--- theia/sentinel/urls.py
+++ theia/sentinel/urls.py
@@ -3,4 +3,8 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("servers/", views.server_overview, name="server_overview"),
+    path("servers/<int:server_id>/", views.server_view, name="server_view"),
+]
--- theia/sentinel/views.py
+++ theia/sentinel/views.py
@@ -1,17 +1,33 @@
 # -*- coding: utf-8 -*-
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render
+from sentinel.models import AlertEndpoint
+from sentinel.models import AlertLog
+from sentinel.models import ContactGroup
+from sentinel.models import ProfileChangelog
+from sentinel.models import Server
+from sentinel.models import ServerProfile
 
 # Create your views here.
 def index(request):
-    return render(request, "sentinel/layout.html", context={})
+    context = {"page_title": "Index"}
+    return render(request, "sentinel/index.html", context=context)
 
 
 def server_overview(request):
-    pass
+    servers = Server.objects.all()
+    changelog = ProfileChangelog.objects.all().order_by("-date_modified")[:10]
+    context = {"servers": servers, "changelog": changelog}
+    return render(request, "sentinel/server_overview.html", context=context)
 
 
-def server_view():
-    pass
+def server_view(request, server_id: int):
+    server = get_object_or_404(Server, id=server_id)
+    changelog = ProfileChangelog.objects.filter(server=server.id).order_by(
+        "-date_modified"
+    )
+    context = {"server": server, "changelog": changelog}
+    return render(request, "sentinel/server_view.html", context=context)
 
 
 def server_logs():
