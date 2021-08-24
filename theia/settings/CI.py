8861d036798e4c4a6ed6add0a9a4240095b4539a
Calem Roelofs <calemroelofs@outlook.ie>
2021-08-24 08:50:53 +0000

    Added CI settings to project

---
+++ theia/settings/CI.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from .base import *
+from theia.settings.dev import ALLOWED_HOSTS
+
+DATABASES = {"default": {"NAME": ":memory:"}}
+SECRET_KEY = "CI-SECRET-KEY"
+DEBUG = False
+ALLOWED_HOSTS = ["*"]
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+# Celery Beat Settings
+CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
+CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
